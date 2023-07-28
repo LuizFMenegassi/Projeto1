0577bac42db6886443663055d5d926c2b725ffa4
unknown <LuizFMenegassi>
2023-07-28 12:32:24 -0300

    alterando codigo

---
+++ src/telas/cesta.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Image, Dimensions, Text } from 'react-native';
+
+import topo from '../../assets/topo.png';
+
+const width = Dimensions.get("screen").width;
+
+export default function Cesta(){
+    return<> 
+    <Image source={topo} style={estilos.topo}/> 
+    <Text style={estilos.titulo}>Detalhe da Cesta</Text>
+    </>
+}
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 578 / 768 *width,
+    },
+    titulo:{
+        width: "100%",
+        position: "absolute",
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight: 26,
+        color: "white",
+        fontWeight: "bold",
+        padding: 16
+    }
+});
+
