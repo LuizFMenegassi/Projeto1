a186dd3053ce8fa9a8ab178b2f4abcf475b2f804
unknown <LuizFMenegassi>
2023-07-28 14:05:24 -0300

    novo arquivo

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { StatusBar, SafeAreaView} from 'react-native';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold,
+  Montserrat_400Regular_Italic 
+} from '@expo-google-fonts/montserrat';
+
 import Cesta from './src/telas/cesta';
 
 export default function App() {
+  const [fontecarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
   return (
     <SafeAreaView>
       <StatusBar/>
